5740138d9d4b631ee47cbc8dc69b1ba55328033b
Bianca <67375556+tichyblazon@users.noreply.github.com>
2026-03-24 18:55:50 +0100

    add new dataset + filters, graphs

--- Appka/app_pages/home_page.py
+++ Appka/app_pages/home_page.py
@@ -4,24 +4,26 @@ import pandas as pd
 import plotly.express as px
 import os
 
+st.set_page_config(page_title="Analýza trhu práce SK", page_icon="📈", layout="wide", initial_sidebar_state="collapsed")
+
+st.title("Analýza Nezamestnanosti na Slovensku")
+st.caption("Interaktívna analýza nezamestnanosti v rokoch 2005–2025 podľa regiónu, pohlavia, veku a vzdelania")
+
+#DATASETY
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 df = pd.read_csv(os.path.join(BASE_DIR, "..", "dataset.csv"))
+df_odvetvie = pd.read_csv(os.path.join(BASE_DIR, "..", "dataset_odvetvie.csv"))
 
 #z wide formatu na long format
 df_melted = df.melt(id_vars = ["region", "pohlavie","typ_skupiny", "skupina"], var_name = "rok", value_name = "nezamestnanost")
 df_melted["rok"] = df_melted["rok"].astype(int)
-
-
-st.set_page_config(page_title="Analýza trhu práce SK", page_icon="📈", layout="wide", initial_sidebar_state="collapsed")
-
-st.title("Analýza Nezamestnanosti na Slovensku")
-st.caption("Interaktívna analýza nezamestnanosti na Slovensku v rokoch 2005–2025 podľa regiónu, pohlavia, veku a vzdelania")
-###
-#ZACIATOK sidebar    
 df_sr = df_melted[df_melted["region"] == "Slovenská republika"]
 
-
-st.sidebar.header("Vyber si filter")
+#desatinna ciarka v druhom datasete, nahradit a pretypovat stlpec
+df_odvetvie["Hodnota"] = df_odvetvie["Hodnota"].str.replace(",", ".").replace("-", float("nan")).astype(float)
+###
+#ZACIATOK sidebar 
+st.sidebar.header("Vyber si filter pre prvý dataset")
 
 selected_region = st.sidebar.multiselect("Zvol region",options=df_melted["region"].unique(), default=df_sr["region"].unique())
 selected_pohlavie = st.sidebar.multiselect("Zvol pohlavie",options=df_melted["pohlavie"].unique(), default=df_melted["pohlavie"].unique())
@@ -33,7 +35,6 @@ if selected_typ == "vek":
 else:
     selected_skupina = st.sidebar.multiselect("Zvol vzdelanie", options=df_melted[df_melted["typ_skupiny"] == "vzdelanie"]["skupina"].unique(), default=["Vysokoškolské"])
 
-
 filtered_df = df_melted[
     (df_melted["region"].isin(selected_region)) &
     (df_melted["pohlavie"].isin(selected_pohlavie)) &
@@ -41,10 +42,27 @@ filtered_df = df_melted[
     (df_melted["rok"].between(selected_roky[0], selected_roky[1]))
 ]
 
+
+
+st.sidebar.divider()
+st.sidebar.header("Vyber si filtre pre druhy dataset")
+#filtre pre druhy dataset
+selected_pohlavie_odvetvie = st.sidebar.multiselect("Zvol pohlavie", options=df_odvetvie["Pohlavie"].unique(), default=["Spolu"])
+selected_odvetvie = st.sidebar.multiselect("Zvol odvetvie", options=df_odvetvie["Odvetvie"].unique(), default=["M Odborné, vedecké a technické činnosti"])
+selected_roky_odvetvie = st.sidebar.slider("Zvol roky", min_value = df_odvetvie["Rok"].min(), max_value = df_odvetvie["Rok"].max(), value = (2008, 2025), step = 1)
+
+filtered_df_odvetvie = df_odvetvie[
+    (df_odvetvie["Pohlavie"].isin(selected_pohlavie_odvetvie)) &
+    (df_odvetvie["Odvetvie"].isin(selected_odvetvie)) &
+    (df_odvetvie["Rok"].between(selected_roky_odvetvie[0], selected_roky_odvetvie[1]))
+]
+
 #KONIEC SIDEBARU
 ###
 
 ###
+#MAIN    
+st.write(df_odvetvie.head())
 #STATISTIKY
 #1 statistika
 najhorsi_region = filtered_df.groupby("region")["nezamestnanost"].mean().idxmax()
@@ -66,34 +84,27 @@ col2.metric(label = "Priemerná Nezamestnanost v priebehu rokov na Slovensku na 
 col3.metric(label = "Gender gap", value = gender_gap, border=True, format = "percent")
 
 #tu je error ked je nan hodnota a vyriesit tie 2020,5 a 2021,5 v datasetu, kedze su to priemery a nie realne roky, tak ich nahradit za 2020 a 2021, to sa da v update_layout urobit, ale skaredo to ukazuje grafy
-#to lano v line grafe je len vtedy ak je viacero regionov, inak je to len jedna ciara a nevyzera to pekne, takze tam dat podmienku, ze ak je len jeden region, tak line_shape="linear" a ak je viacero regionov, tak line_shape="spline"
 
 ###
-#GRAFY
-
-
+#GRAFY 1 DATASET
 
 if selected_typ == "vek":
     bar_graph_title = "Nezamestnanosť podľa veku v priebehu rokov"
+    boxplot_title = "Rozloženie Nezamestnanosti podľa Veku"
+    skupina = "Vek"
 else:
     bar_graph_title = "Nezamestnanosť podľa vzdelania v priebehu rokov"
+    boxplot_title = "Rozloženie Nezamestnanosti podľa Vzdelania"
+    skupina = "Vzdelanie"
 
 cols_graphs_first = st.columns(1)
 with cols_graphs_first[0]:
     bar_graph_1 = px.bar(filtered_df, x="rok", y="nezamestnanost", color="pohlavie", barmode= "group", color_discrete_sequence=px.colors.qualitative.Set3, title=bar_graph_title, hover_data=["skupina"], facet_col="region")
     #bar_graph_1.update_traces(marker_line_color="grey", marker_line_width=0.4)
+    bar_graph_1.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
     st.plotly_chart(bar_graph_1)
 
-
-
-
-
-
-
-if selected_typ == "vek":
-    boxplot_title = "Rozloženie Nezamestnanosti podľa Veku"
-else:    
-    boxplot_title = "Rozloženie Nezamestnanosti podľa Vzdelania"
+       
 
 cols_graphs_second=st.columns(3)
 with cols_graphs_second[0]:
@@ -105,11 +116,31 @@ with cols_graphs_second[0]:
     
 with cols_graphs_second[1]:  
     boxplot_graph = px.box(filtered_df,x = "skupina", y = "nezamestnanost", color = "pohlavie", color_discrete_sequence=px.colors.qualitative.Set3, facet_col="region", facet_col_spacing = 0.2, title=boxplot_title)
-    boxplot_graph.update_layout(title = boxplot_title, xaxis_title = "Skupina", yaxis_title = "Nezamestnanosť")
+    boxplot_graph.update_layout(title = boxplot_title, xaxis_title = skupina, yaxis_title = "Nezamestnanosť")
+    boxplot_graph.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
     st.plotly_chart(boxplot_graph)
 
 with cols_graphs_second[2]:
     scatter = px.scatter(filtered_df, x = "rok", y = "skupina", color = "nezamestnanost", color_discrete_sequence=px.colors.qualitative.Set3, size = "nezamestnanost", title="Scatter")
-    #scatter.update_layout(xaxis_title="Rok", yaxis_title="Nezamestnanosť")
+    scatter.update_layout(xaxis_title="Rok", yaxis_title=skupina, title="Nezamestnanosť podľa skupiny v priebehu rokov")
     st.plotly_chart(scatter)
     #tu hadze error ohladom nan hodnot pri vzdelani
+
+st.divider()
+###
+#GRAFY 2 DATASET
+
+
+st.subheader("Nezamestnanosť podľa odvetvia")
+st.caption("Analýza Nezamestnanosti na Slovensku v rokoch 2008–2025 podľa pohlavia a odvetvia")
+
+col1, col2, col3 = st.columns(3)
+col1.metric(label = "Region s najväčšou nezamestnanosťou", value = najhorsi_region, border=True)
+col2.metric(label = "Priemerná Nezamestnanost v priebehu rokov na Slovensku na základe veku", value = "2005 - 2025", chart_data=df_sr_vek, chart_type="line", border=True) #nejako pospekulovat
+col3.metric(label = "Gender gap", value = gender_gap, border=True, format = "percent")
+
+cols_graphs_third = st.columns(1)
+with cols_graphs_third[0]:
+    line_graph = px.line(filtered_df_odvetvie, x="Rok", y="Hodnota", color="Odvetvie", line_dash="Pohlavie", color_discrete_sequence=px.colors.qualitative.Set3, facet_row="Pohlavie", facet_row_spacing= 0.2, title="Nezamestnanosť podľa odvetvia v priebehu rokov")
+    line_graph.update_layout(xaxis_title = "Roky", yaxis_title = "Nezamestnanosť")
+    st.plotly_chart(line_graph)
