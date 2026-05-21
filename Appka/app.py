9670efc41fa63857a58f269bb091c022565ea0dd
Bianca <67375556+tichyblazon@users.noreply.github.com>
2026-05-21 11:17:33 +0200

    Update app.py

--- Appka/app.py
+++ Appka/app.py
@@ -5,4 +5,3 @@ pg = st.navigation([st.Page("app_pages/home_page.py", title = "Domov"),
                     st.Page("app_pages/predikcia_page.py", title = "Predikcia")], position="top")
 pg.run()
 
-#py -m streamlit run app.py
